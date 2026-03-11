3c25f7e70c8db67ef80d200a651f64fbb0ef9109
pxrxp <187556078+pxrxp@users.noreply.github.com>
2026-03-11 10:35:15 +0545

    Edit gru.py to add GRU wrapper for sequence processing

--- modules/gru.py
+++ modules/gru.py
@@ -26,3 +26,24 @@ class GRUCell(Module):
         h_next = (1 - z) * h + z * h_tilde
         
         return h_next
+
+class GRU(Module):
+    def __init__(self, input_size, hidden_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.cell = GRUCell(input_size, hidden_size)
+
+    def forward(self, x, h0=None):
+        seq_len, batch_size, _ = x.size()
+        
+        if h0 is None:
+            h = torch.zeros(batch_size, self.hidden_size, device=x.device)
+        else:
+            h = h0
+        
+        outputs = []
+        for i in range(seq_len):
+            h = self.cell(x[i], h)
+            outputs.append(h)
+            
+        return torch.stack(outputs), h
