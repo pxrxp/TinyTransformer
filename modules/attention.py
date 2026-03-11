fd0adcd5b207bb4b8cebfaa6c32f0a421b30c46d
pxrxp <187556078+pxrxp@users.noreply.github.com>
2026-03-11 16:37:07 +0545

    Edit attention.py to implement MultiHeadAttention

--- modules/attention.py
+++ modules/attention.py
@@ -24,3 +24,43 @@ class ScaledDotProductAttention(Module):
         
         # 3. Matmul: (L x L) weights * (L x d_v) values = (L x d_v) context
         return torch.matmul(weights, v), weights
+
+class MultiHeadAttention(Module):
+    def __init__(self, d_model, num_heads):
+        super().__init__()
+        assert d_model % num_heads == 0
+        
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.d_k = d_model // num_heads
+        
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        self.w_o = nn.Linear(d_model, d_model)
+        
+        self.attention = ScaledDotProductAttention()
+
+    def split_heads(self, x):
+        batch, seq_len, _ = x.size()
+        # view(): Breaks d_model into (Heads, d_k). Shape: (B, L, H, d_k)
+        # transpose(1, 2): Swaps L and H -> (B, H, L, d_k)
+        # This makes it a 'stack' of H matrices, each of size (L x d_k).
+        return x.view(batch, seq_len, self.num_heads, self.d_k).transpose(1, 2)
+
+    def combine_heads(self, x):
+        batch, heads, seq_len, d_k = x.size()
+        # transpose(1, 2): Swaps back -> (B, L, H, d_k)
+        # contiguous(): Re-orders memory so the H and d_k dimensions are side-by-side
+        # view(): Glues (H, d_k) back into d_model -> (B, L, d_model)
+        return x.transpose(1, 2).contiguous().view(batch, seq_len, self.d_model)
+
+    def forward(self, q, k, v, mask=None):
+        q, k, v = self.w_q(q), self.w_k(k), self.w_v(v)
+        
+        q, k, v = self.split_heads(q), self.split_heads(k), self.split_heads(v)
+        
+        context, weights = self.attention(q, k, v, mask)
+        
+        out = self.combine_heads(context)
+        return self.w_o(out), weights
